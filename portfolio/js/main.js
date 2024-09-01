63d05df16e49a9396904b726f7177c69a09a688b
Rabeh3mk <abderrahmanrabeh03@gmail.com>
2024-09-01 23:50:03 +0100

    complete

--- portfolio/js/main.js
+++ portfolio/js/main.js
@@ -6,3 +6,33 @@ function openMenu() {
 function closeMenu() {
   sideMenu.style.transform = "translateX(16rem)";
 }
+
+window.addEventListener("scroll", function () {
+  if (scrollY > 50) {
+    document
+      .querySelector("nav")
+      .classList.add(
+        "bg-white",
+        "bg-opacity-50",
+        "backdrop-blur-lg",
+        "shadow-sm"
+      );
+
+    document
+      .querySelector("nav ul")
+      .classList.remove("bg-white", "shadow-sm", "bg-opacity-50");
+  } else {
+    document
+      .querySelector("nav")
+      .classList.remove(
+        "bg-white",
+        "bg-opacity-50",
+        "backdrop-blur-lg",
+        "shadow-sm"
+      );
+
+    document
+      .querySelector("nav ul")
+      .classList.add("bg-white", "shadow-sm", "bg-opacity-50");
+  }
+});
